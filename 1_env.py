db732a09917da5c6c5b1b661b6b6fc7675d1180c
shinjaewon99 <95893341+shinjaewon99@users.noreply.github.com>
2022-10-02 21:23:03 +0900

    Add files via upload

---
+++ 1_env.py
@@ -0,0 +1,9 @@
+import pyautogui
+
+
+size = pyautogui.size() # 현재 화면의 스크린 사이즈를 가져옴
+
+print(size) # 가로 , 세로 크기를 알수있음
+
+# size[0] : width
+# size[1] : height
---
+++ 3_mouse_action.py
@@ -0,0 +1,24 @@
+from pydoc import cli
+import pyautogui
+
+# pyautogui.sleep(3) # 3초 대기
+
+# print(pyautogui.position())
+
+# pyautogui.click(81,21 , duration=1) # 1초동안  (81,21) 좌표로 이동후 마우스 클릭
+
+# pyautogui.mouseDown()
+# pyautogui.mouseUp()
+
+# pyautogui.doubleClick()
+# pyautogui.click(clicks=2)
+
+
+# pyautogui.moveTo(200,200)
+# pyautogui.mouseDown() #마우스 버튼 누른 상태
+# pyautogui.moveTo(300,300)
+# pyautogui.mouseUp() # 마우스 버튼 땐 상태
+
+# pyautogui.middleClick()
+# pyautogui.rightClick()
+# pyautogui.drag()
